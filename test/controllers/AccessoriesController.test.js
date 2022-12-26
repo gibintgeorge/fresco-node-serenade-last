2738251380ff51614fa6b27b2ebd6781979d6600
Gibin T George <gibintgeorge@hotmail.com>
2022-12-26 20:16:25 +0530

    accessories

--- test/controllers/AccessoriesController.test.js
+++ test/controllers/AccessoriesController.test.js
@@ -1,103 +1,134 @@
-var supertest = require('supertest');
-var assert = require('assert');
+var supertest = require("supertest");
+var assert = require("assert");
 
 var createdSegment;
-require('../bootstrap.test');
+require("../bootstrap.test");
 
-var accessories = require('../fixtures/Accessories.json');
+var accessories = require("../fixtures/Accessories.json");
 
-describe('Accessories Controller', function () {
-  it('get /accessories', function (done) {
+describe("Accessories Controller", function () {
+  it("post /accessories", function (done) {
     var agent = supertest.agent(sails.hooks.http.app);
     agent
-      .get('/accessories')
-      .send()
-      .expect(200)
-      .end(function (err, result) {
-        if (err) {
-          done(err);
-        } else {
-          result.body.length.should.be.aboveOrEqual(0);
-          done();
-        }
-      });
-  });
-
-  it('post /accessories', function (done) {
-    var agent = supertest.agent(sails.hooks.http.app);
-    agent
-      .post('/accessories')
+      .post("/accessories")
       .send(accessories)
       .expect(200)
       .end(function (err, result) {
         if (err) {
           done(err);
         } else {
-          result.body.length.should.be.aboveOrEqual(0);
           done();
         }
       });
   });
-
-  it('put /accessories', function (done) {
+  it("get /accessories", function (done) {
     var agent = supertest.agent(sails.hooks.http.app);
     agent
-      .put('/accessories/1')
-      .send(accessories[0])
-      .expect(200)
-      .end(function (err, result) {
-        if (err) {
-          done(err);
-        } else {
-          result.body.length.should.be.aboveOrEqual(0);
-          done();
-        }
-      });
-  });
-
-  it('delete /accessories', function (done) {
-    var agent = supertest.agent(sails.hooks.http.app);
-    agent
-      .delete('/accessories/1')
-      .send(accessories[0])
-      .expect(200)
-      .end(function (err, result) {
-        if (err) {
-          done(err);
-        } else {
-          result.body.length.should.be.aboveOrEqual(0);
-          done();
-        }
-      });
-  });
-
-  it('deleteAll /accessories', function (done) {
-    var agent = supertest.agent(sails.hooks.http.app);
-    agent
-      .delete('/accessories')
+      .get("/accessories")
       .send()
       .expect(200)
       .end(function (err, result) {
         if (err) {
           done(err);
         } else {
-          result.body.length.should.be.aboveOrEqual(0);
           done();
         }
       });
   });
-
-  it('search /accessories', function (done) {
+  it("put /accessories", function (done) {
     var agent = supertest.agent(sails.hooks.http.app);
     agent
-      .get('/accessories/search')
+      .put("/accessories/1")
+      .send(accessories[0])
+      .expect(200)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+  it("put /accessories invalid", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .put("/accessories/ids")
+      .send(accessories[0])
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+  it("search /accessories", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .get("/accessories/search?limit=1&skip=5&sort=price&where1=1")
       .send()
       .expect(200)
       .end(function (err, result) {
         if (err) {
           done(err);
         } else {
-          result.body.length.should.be.aboveOrEqual(0);
+          done();
+        }
+      });
+  });
+  it("delete /accessories", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete("/accessories/1")
+      .send(accessories[0])
+      .expect(200)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+  it("delete /accessories invalid id", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete("/accessories/ABC")
+      .send(accessories[0])
+      .expect(400)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+  it("delete /accessories huge invalid id", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete("/accessories/10000")
+      .send(accessories[0])
+      .expect(404)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+  it("deleteAll /accessories", function (done) {
+    var agent = supertest.agent(sails.hooks.http.app);
+    agent
+      .delete("/accessories")
+      .send()
+      .expect(200)
+      .end(function (err, result) {
+        if (err) {
+          done(err);
+        } else {
           done();
         }
       });
